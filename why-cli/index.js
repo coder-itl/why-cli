a99e36ff04d0455c9b0620db47b75e7b4e6b6b4c
lovobin <3327511395@qq.com>
2021-06-19 16:11:10 +0800

    feat:add new options

--- why-cli/index.js
+++ why-cli/index.js
@@ -7,6 +7,16 @@ program.version(require('./package.json').version)
 
 // 增加自己的 options
 program.option('-w --why', 'a why cli')
+program.option(
+  '-d --dest<dest>',
+  'a destination folder，Eg: -d /src/components',
+)
+program.option('-f --framework<framework>', 'choice you framework')
 
+program.on('--help', function () {
+  console.log('')
+  console.log('Others: ')
+  console.log('  other options~')
+})
 // 解析
 program.parse(process.argv)
